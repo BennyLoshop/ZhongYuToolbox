1340b7af61d172d83dd85fd521fd85999a6687f9
BennyLoshop <84132229+BennyLoshop@users.noreply.github.com>
2025-10-06 20:45:09 +0800

    Changes to be committed: 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -112,10 +112,11 @@
     }
 
 
-    showGg(`<p><b>公告</b></p>
-<p>网站新域名上线：zytb.loshop.com.cn</p>
-<p>QQ群：1067807011</p>
-`);
+//     showGg(`<p><b>公告</b></p>
+// <p>网站新域名上线：zytb.loshop.com.cn</p>
+// <p>QQ群：1067807011</p>
+// `);
+
 })();
 
 let lastProxyStatus = null;
